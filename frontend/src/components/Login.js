9c4b07adcfad9f087812394fdaf6ceb8c43d6410
Alex Chung <AlexDChung@ibm.com>
2021-03-07 11:43:36 -0500

    CORS enabled for local dev

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,11 +5,32 @@ import {refreshTokenSetup} from "../utils/TokenRefresh";
 
 const clientId = '838132247517-n9cve8sucts7h0cp93bhjifogr3i4k88.apps.googleusercontent.com'
 
+
+
+
 function Login() {
-    const onSuccess = (res) => {
-        console.log('[Login Success] currentUser: ', res.profileObj)
-        refreshTokenSetup(res);
-    };
+    // const onSuccess = (res) => {
+    //     console.log('[Login Success] currentUser: ', res.profileObj)
+    //     refreshTokenSetup(res);
+    // };
+
+    const handleLogin = async googleData => {
+        console.log(googleData.tokenId)
+        const res = await fetch("http://localhost:5000/api/auth/google", {
+            method: "POST",
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        })
+        const data = await res.json()
+        console.log(data)
+        // store returned user in a context?
+    }
+
 
     const onFailure = (res) => {
         console.log('[Login Failed] res:', res)
@@ -24,7 +45,7 @@ function Login() {
                              </button>
                          )}
                          buttonText="Login"
-                         onSuccess={onSuccess}
+                         onSuccess={handleLogin}
                          onFailure={onFailure}
                          cookiePolicy={'single_host_origin'}
                          style={{marginTop: '100px'}}
